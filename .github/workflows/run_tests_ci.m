8662549c562bec697483dfb94772944747a6a8c8
Remi Gau <remi_gau@hotmail.com>
2022-05-07 14:03:11 +0200

    add matlab test in ci

---
+++ .github/workflows/run_tests_ci.m
@@ -0,0 +1,12 @@
+% run tests with code coverage via the run_tests scripts in the root folder.
+% (C) Copyright 2022 Remi Gau
+
+root_dir = getenv('GITHUB_WORKSPACE');
+
+% MOxUnit and MOcov need to be in the matlab path
+addpath(fullfile(root_dir, 'MOcov', 'MOcov'));
+cd(fullfile(root_dir, 'MOxUnit', 'MOxUnit'));
+run moxunit_set_path();
+
+cd(root_dir);
+run run_tests();
